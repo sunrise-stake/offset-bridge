2bbf6a2748618b19bb6da2618a1e5f2667f6db47
Lilly <reubenh14@tutanota.com>
2023-06-05 01:32:25 +0200

    correct mainnet addresses

--- constants/index.ts
+++ constants/index.ts
@@ -1,13 +1,13 @@
-// wormhole contract addresses mainnet + TEST net
 // reference: https://book.wormhole.com/reference/contracts.html
 
-const POLYGON_TEST_TOKEN_BRIDGE_ADDRESS = "0x377D55a7928c046E18eEbb61977e714d2a76472a";
-const SOL_TEST_TOKEN_BRIDGE_ADDRESS = "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe";
+const POLYGON_TOKEN_BRIDGE_ADDRESS = "0x5a58505a96D1dbf8dF91cB21B54419FC36e93fdE"; //mainnet
+const SOL_TOKEN_BRIDGE_ADDRESS = "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb";
+const SOL_BRIDGE_ADDRESS = "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth";
+
+const SOL_TEST_TOKEN_BRIDGE_ADDRESS = "DZnkkTmCiFWfYTfT41X3Rd1kDgozqzxWaHqsw6W4x2oe"; //devnet
 //"B6RHG3mfcckmrYN1UhmJzyS1XX3fZKbkeUcpJe9Sy3FE"; 
 const SOL_TEST_BRIDGE_ADDRESS = "3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5";
 //"Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o";
-const SOL_TOKEN_BRIDGE_ADDRESS = "wormDTUJ6AWPNvk59vGQbDvGJmqbDTdgWgAqcLBCgUb";
-const SOL_BRIDGE_ADDRESS = "worm2ZoG2kUd4vFXhvjh93UUH596ayRfgQ2MgjNMTth";
 const CHAIN_ID_POLYGON = 5;
 const CHAIN_ID_SOLANA = 1;
 
@@ -15,8 +15,8 @@ const CHAIN_ID_SOLANA = 1;
 // const USDC_TEST_TOKEN = "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU";
 const USDC_TEST_TOKEN = "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr"; // interface: https://spl-token-faucet.com/?token-name=USDC
 
-// Holding contract deployed on sepolia testnet 
-const TARGET_CONTRACT = "0xFed3CfC8Ea0bF293e499565b8ccdD46ff8B37Ccb"; // this is just any contract rn 
+// Holding contract deployed on Polygon mainnet 
+const TARGET_CONTRACT = "0x7022404732CB3ec5aC95c2c75080A76226AA74F5";
 
 const WORMHOLE_RPC_HOST = ["http://guardian:7071"];
 // const WORMHOLE_RPC_HOST = ["https://wormhole-v2-testnet-api.certus.one"];
