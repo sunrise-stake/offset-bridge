6c8f03ddbfa10d17006f13314de386a577eda7ac
dankelleher <dankelleher@yahoo.com>
2024-08-16 14:28:38 +0200

    Add script to create holding contracts on Polygon Adapt the initialize script on SOL to support the initialize function change

--- scripts/initialize.ts
+++ scripts/initialize.ts
@@ -3,18 +3,26 @@ import {
     PublicKey,
     Keypair
 } from '@solana/web3.js';
-import { BRIDGE_INPUT_MINT_ADDRESS, SOLANA_RPC_ENDPOINT, USER_KEYPAIR} from "./constants";
+import {BRIDGE_INPUT_MINT_ADDRESS, CHAIN_ID_POLYGON, SOLANA_RPC_ENDPOINT, USER_KEYPAIR} from "./constants";
 import {Program, AnchorProvider } from "@coral-xyz/anchor";
 import {IDL, TokenSwap} from "./types/token_swap";
 import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import {ethers} from "ethers";
 
 const PROGRAM_ID = new PublicKey("suobUdMc9nSaQ1TjRkQA4K6CR9CmDiU9QViN7kVw74T");
+
+// Read the holding contract address from the command line
+const holdingContract = process.argv[2];
+// ensure the holding contract is provided and is a valid ethereum address
+if (!holdingContract || !!ethers.utils.isAddress(holdingContract)) {
+    console.error('Usage: bun run scripts/initialize.ts <holdingContract>');
+    process.exit(1);
+}
 
 (async () => {
     // Setup Solana RPC connection
     const connection = new Connection(SOLANA_RPC_ENDPOINT);
 
-    // execute the swap transaction
     const provider = new AnchorProvider(connection, new NodeWallet(USER_KEYPAIR), {});
     const program = new Program<TokenSwap>(IDL, PROGRAM_ID, provider);
 
@@ -23,8 +31,12 @@ const PROGRAM_ID = new PublicKey("suobUdMc9nSaQ1TjRkQA4K6CR9CmDiU9QViN7kVw74T");
 
     const outputMint = new PublicKey(BRIDGE_INPUT_MINT_ADDRESS);
 
-    const txid = await program.methods.initialize(
-        outputMint
+    const txid = await program.methods.initialize({
+        outputMint,
+        holdingContract,
+        tokenChainId: CHAIN_ID_POLYGON,
+        updateAuthority: USER_KEYPAIR.publicKey,
+        }
     ).accounts({
         state: stateAddress.publicKey,
     }).signers([stateAddress]).rpc()
---
+++ scripts/sendMatic.ts
@@ -0,0 +1,58 @@
+import { ContractReceipt, ethers } from "ethers";
+
+import {HOLDING_CONTRACT_ABI} from "../retirement-ui/src/lib/abi/holdingContract";
+import {
+    HOLDING_CONTRACT_FACTORY_ADDRESS,
+    holdingContractFactorySalt,
+    RETIREMENT_CONTRACT
+} from "../retirement-ui/src/lib/constants";
+import {HOLDING_CONTRACT_FACTORY_ABI} from "../retirement-ui/src/lib/abi/holdingContractFactory";
+import {PublicKey} from "@solana/web3.js";
+import {solanaAddressToHex} from "./util";
+
+require("dotenv").config(({ path: ".env" }));
+const POLYGON_PRIVATE_KEY = process.env.POLYGON_PRIVATE_KEY;
+const POLYGON_NODE_URL = process.env.POLYGON_NODE_URL;
+
+const ethProvider = new ethers.providers.JsonRpcProvider(POLYGON_NODE_URL);
+const ethSigner = new ethers.Wallet(POLYGON_PRIVATE_KEY, ethProvider);
+
+const targetAddress = process.argv[2];
+if (!targetAddress || !ethers.utils.isAddress(targetAddress)) {
+    console.error('Usage: bun run scripts/sendMatic.ts <targetAddress>');
+    process.exit(1);
+}
+
+
+
+async function main() {
+    const gasPrice = ethers.utils.parseUnits('30', 'gwei');
+    const gasLimit = 21000;
+
+    // Get the wallet's balance
+    const balance = await ethSigner.getBalance();
+
+    // Calculate the transaction fee
+    const txFee = gasPrice.mul(gasLimit);
+
+    // Calculate the amount to send (total balance minus the fee)
+    const valueToSend = balance.sub(txFee);
+
+    // Create the transaction object
+    const tx = {
+        to: targetAddress,
+        value: valueToSend,
+        gasPrice: gasPrice,
+        gasLimit: gasLimit,
+        nonce: 0 // Use nonce 0 as requested
+    };
+
+    // Send the transaction
+    const transaction = await ethSigner.sendTransaction(tx);
+
+    console.log('Transaction:', transaction.hash);
+    await transaction.wait();
+    console.log('Transaction confirmed');
+}
+
+main().catch(console.error);
