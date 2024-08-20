b1c1ce860af6dd917017141ab19843a74ad31e92
gigileungyingchi <lycgigi2020@gmail.com>
2024-08-20 14:27:52 +0200

    fixup! Add tests for updated program

--- token-swap/tests/token-swap.ts
+++ token-swap/tests/token-swap.ts
@@ -1,11 +1,13 @@
 import { Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
-import { Connection, PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey, TransactionInstruction } from "@solana/web3.js";
 import { TokenSwap, IDL } from "../../scripts/types/token_swap";
-import { USER_KEYPAIR, CHAIN_ID_POLYGON, SOL_TOKEN_BRIDGE_ADDRESS } from "../../scripts/constants";
+import { USER_KEYPAIR, CHAIN_ID_POLYGON, SOL_TOKEN_BRIDGE_ADDRESS, BRIDGE_INPUT_MINT_ADDRESS, SOL_BRIDGE_ADDRESS, USDC_TOKEN_POLYGON } from "../../scripts/constants";
 import { HOLDING_CONTRACT_FACTORY_ADDRESS, holdingContractFactorySalt } from "../../retirement-ui/src/lib/constants";
 import { HOLDING_CONTRACT_FACTORY_ABI  } from "../../retirement-ui/src/lib/abi/holdingContractFactory";
-import { bridgeAuthority, bridgeInputTokenAccount } from "../../scripts/util";
+// import { bridgeAuthority, bridgeInputTokenAccount } from "../../scripts/util";
+import { Account, getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
 import * as Wormhole from "@certusone/wormhole-sdk";
 
 export const PROGRAM_ID = new PublicKey(
@@ -18,16 +20,62 @@ export const OUTPUT_MINT = new PublicKey("E2VmbootbVCBkMNNxKQgCLMS1X3NoGMaYAsufa
 export const INBETWEEN_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
 export const WRAPPED_SOL_TOKEN_MINT = new PublicKey("So11111111111111111111111111111111111111112");
 export const HOLDING_CONTRACT_ADDRESS = "";
-import { createWormholeWrappedTransfer } from "../../scripts/bridge";
+// import { createWormholeWrappedTransfer } from "../../scripts/bridge";
+import { createTransferWrappedInstruction } from "@certusone/wormhole-sdk/lib/cjs/solana/tokenBridge";
 import { BN } from "bn.js";
-import { TOKEN_PROGRAM_ID } from "spl-token-latest";
+// import { TOKEN_PROGRAM_ID } from "spl-token-latest";
 import { createPublicClient, http } from "viem";
 import { polygon } from "viem/chains";
- 
+import { Address } from "viem";
+import { expect } from "chai";
+import * as chai from "chai";
+
 const publicClient = createPublicClient({ 
   chain: polygon,
   transport: http()
 });
+
+type TransferTransactionInformation = {
+  instruction: TransactionInstruction;
+  message: Keypair;
+}
+
+export const createWormholeWrappedTransfer = async (
+  payerAddress: PublicKey,
+  fromAddress: PublicKey,
+  tokenAuthority: PublicKey,
+  amount: bigint,
+  targetAddress: Uint8Array,
+): Promise<TransferTransactionInformation> => {
+  const nonce = Wormhole.createNonce().readUInt32LE(0);
+  const message = Keypair.generate();
+  const tokenBridgeTransferIx = createTransferWrappedInstruction(
+    SOL_TOKEN_BRIDGE_ADDRESS,
+    SOL_BRIDGE_ADDRESS,
+    payerAddress,
+    message.publicKey,
+    fromAddress,
+    tokenAuthority,
+    CHAIN_ID_POLYGON, // The origin chain of the wrapped token is polygon
+    // USDC_TOKEN_POLYGON,
+    Wormhole.tryNativeToUint8Array(USDC_TOKEN_POLYGON, CHAIN_ID_POLYGON), // The origin address of the wrapped token is the USDC token address on polygon
+    nonce,
+    amount,
+    0n,
+    targetAddress,
+    CHAIN_ID_POLYGON
+  );
+
+  // the token authority is a PDA. the instruction be "signed2 by the program when sent to wormhole via CPI.
+  // so we don't want an explicit signer here
+  tokenBridgeTransferIx.keys.find((key) => key.pubkey.equals(tokenAuthority))!.isSigner = false;
+
+  console.log("payer: ", payerAddress.toBase58())
+  console.log("tokenAuthority: ", tokenAuthority.toBase58())
+  console.log("message: ", message.publicKey.toBase58())
+
+  return { instruction: tokenBridgeTransferIx, message }
+}
 
 describe("token-swap", () => {
   // Configure the client to use the local cluster.
@@ -43,69 +91,149 @@ describe("token-swap", () => {
   const program = new Program<TokenSwap>(IDL, PROGRAM_ID, provider);
   const tokenAuthority = PublicKey.findProgramAddressSync([Buffer.from("token_authority"), stateAddress.toBuffer()], PROGRAM_ID)[0];
   console.log("program set");
+  // const bridgeInputTokenAccount = getAssociatedTokenAddressSync(new PublicKey(BRIDGE_INPUT_MINT_ADDRESS), tokenAuthority, true);
+  const bridgeAuthority = PublicKey.findProgramAddressSync([Buffer.from("authority_signer")], new PublicKey(SOL_TOKEN_BRIDGE_ADDRESS))[0];
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx1 = await connection.requestAirdrop(
-      USER_KEYPAIR.publicKey,
-      LAMPORTS_PER_SOL
-    );
-    const blockhash1 = await connection.getLatestBlockhash();
-    await connection.confirmTransaction({ signature: tx1, ...blockhash1 });
-    const tx2 = await connection.requestAirdrop(
-      stateAddress,
-      LAMPORTS_PER_SOL
-    );
-    const blockhash2 = await connection.getLatestBlockhash();
-    await connection.confirmTransaction({ signature: tx2, ...blockhash2 });
-    console.log("airdrop done")
+  context("Create and test bridge", () => {
+    let holdingContractAddress: Address | undefined = undefined;
+    let wormholeTargetAddress: Uint8Array;
 
-    const tx = await program.methods.initialize({
-        outputMint: OUTPUT_MINT,
-        holdingContract: HOLDING_CONTRACT_ADDRESS,
-        tokenChainId: CHAIN_ID_POLYGON.toString(),
-        updateAuthority: USER_KEYPAIR.publicKey
-    }).accounts({ state: stateAddress, authority: USER_KEYPAIR.publicKey })
-    .signers([stateKeypair ])
-    .rpc()
-    .catch((e) => {
-      console.log(e.logs);
-      throw e;
-    });
-    console.log("Your transaction signature", tx);
-  });
+    beforeEach(async () => {
+      const salt = holdingContractFactorySalt(USER_KEYPAIR.publicKey.toString());
 
-  it("Target in bridge data match state holding contract", async () => {
-    const amount = BigInt(1);
-    const salt = holdingContractFactorySalt(USER_KEYPAIR.publicKey.toString());
+      const holdingContractFactoryGetContractRead = await publicClient.readContract({
+        address: HOLDING_CONTRACT_FACTORY_ADDRESS,
+        abi: HOLDING_CONTRACT_FACTORY_ABI,
+        functionName: "getContractAddress",
+        args: [ salt, HOLDING_CONTRACT_FACTORY_ADDRESS ]
+      });
 
-    const holdingContractFactoryGetContractRead = await publicClient.readContract({
-      address: HOLDING_CONTRACT_FACTORY_ADDRESS,
-      abi: HOLDING_CONTRACT_FACTORY_ABI,
-      functionName: "getContractAddress",
-      args: [ salt, HOLDING_CONTRACT_FACTORY_ADDRESS ]
+      holdingContractAddress = holdingContractFactoryGetContractRead  ? holdingContractFactoryGetContractRead : undefined;
+      if (!holdingContractAddress) return;
+      wormholeTargetAddress = Wormhole.tryNativeToUint8Array(holdingContractAddress , CHAIN_ID_POLYGON);
+
     });
 
-    const holdingContractAddress = holdingContractFactoryGetContractRead  ? holdingContractFactoryGetContractRead : undefined;
-    if (!holdingContractAddress) return;
+      
 
-    const { instruction, message } = await createWormholeWrappedTransfer(
-      USER_KEYPAIR.publicKey,
-      bridgeInputTokenAccount,
-      tokenAuthority,
-      amount,
-      Wormhole.tryNativeToUint8Array("" , CHAIN_ID_POLYGON),
-    );
-    const tx = await program.methods.bridge(new BN(amount.toString()), instruction.data).accounts({
-        state: stateAddress,
-        bridgeAuthority,
-        tokenAccountAuthority: tokenAuthority,
-        tokenAccount: bridgeInputTokenAccount,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        wormholeProgram: SOL_TOKEN_BRIDGE_ADDRESS,
-    }).remainingAccounts(instruction.keys)
-      .signers([message]) // appears to get erased when calling transaction()  - TODO remove?
-      .transaction();
+    it("Is initialized!", async () => {
+      // Add your test here.
+      const tx1 = await connection.requestAirdrop(
+        USER_KEYPAIR.publicKey,
+        LAMPORTS_PER_SOL
+      );
+      const blockhash1 = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({ signature: tx1, ...blockhash1 });
+      const tx2 = await connection.requestAirdrop(
+        stateAddress,
+        LAMPORTS_PER_SOL
+      );
+      const blockhash2 = await connection.getLatestBlockhash();
+      await connection.confirmTransaction({ signature: tx2, ...blockhash2 });
+      console.log("airdrop done")
+      console.log(holdingContractAddress);
+      if (!holdingContractAddress) return;
+
+      const tx = await program.methods.initialize({
+          outputMint: OUTPUT_MINT,
+          holdingContract: holdingContractAddress.toString(),
+          tokenChainId: CHAIN_ID_POLYGON.toString(),
+          updateAuthority: USER_KEYPAIR.publicKey
+      }).accounts({ state: stateAddress, authority: USER_KEYPAIR.publicKey })
+      .signers([stateKeypair ])
+      .rpc()
+      .catch((e) => {
+        console.log(e.logs);
+        throw e;
+      });
+      console.log("Your transaction signature", tx);
     });
-  
-  });
+
+    it("Target in bridge data match state holding contract", async () => {
+      const amount = BigInt(1);
+      const bridgeInputTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        USER_KEYPAIR,
+        new PublicKey(BRIDGE_INPUT_MINT_ADDRESS),
+        tokenAuthority,
+        true
+      );
+      const { instruction, message } = await createWormholeWrappedTransfer(
+        USER_KEYPAIR.publicKey,
+        bridgeInputTokenAccount.address,
+        tokenAuthority,
+        amount,
+        wormholeTargetAddress,
+      );
+      const tx = await program.methods.bridge(new BN(amount.toString()), instruction.data).accounts({
+          state: stateAddress,
+          bridgeAuthority,
+          tokenAccountAuthority: tokenAuthority,
+          tokenAccount: bridgeInputTokenAccount.address,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          wormholeProgram: SOL_TOKEN_BRIDGE_ADDRESS,
+      }).remainingAccounts(instruction.keys)
+        .signers([message]) // appears to get erased when calling transaction()  - TODO remove?
+        .transaction();
+
+        try {
+          await provider.sendAndConfirm(tx, [message]);
+        } catch (e) {
+          console.log(e.getLogs());
+          console.log(e);
+        }
+      });
+
+      it("Target in bridge data not matching state holding contract", async () => {
+        const amount = BigInt(1);
+        const bridgeInputTokenAccount = await getOrCreateAssociatedTokenAccount(
+          connection,
+          USER_KEYPAIR,
+          new PublicKey(BRIDGE_INPUT_MINT_ADDRESS),
+          tokenAuthority,
+          true
+        );
+
+        const salt = holdingContractFactorySalt(Keypair.generate().publicKey.toString());
+
+        const holdingContractFactoryGetContractRead = await publicClient.readContract({
+          address: HOLDING_CONTRACT_FACTORY_ADDRESS,
+          abi: HOLDING_CONTRACT_FACTORY_ABI,
+          functionName: "getContractAddress",
+          args: [ salt, HOLDING_CONTRACT_FACTORY_ADDRESS ]
+        });
+
+        const wrongHoldingContractAddress = holdingContractFactoryGetContractRead  ? holdingContractFactoryGetContractRead : undefined;
+        if (!wrongHoldingContractAddress) return;
+        const wrongWormholeTargetAddress = Wormhole.tryNativeToUint8Array(wrongHoldingContractAddress , CHAIN_ID_POLYGON);
+
+    
+        const { instruction, message } = await createWormholeWrappedTransfer(
+          USER_KEYPAIR.publicKey,
+          bridgeInputTokenAccount.address,
+          tokenAuthority,
+          amount,
+          wrongWormholeTargetAddress,
+        );
+        const tx = await program.methods.bridge(new BN(amount.toString()), instruction.data).accounts({
+            state: stateAddress,
+            bridgeAuthority,
+            tokenAccountAuthority: tokenAuthority,
+            tokenAccount: bridgeInputTokenAccount.address,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            wormholeProgram: SOL_TOKEN_BRIDGE_ADDRESS,
+        }).remainingAccounts(instruction.keys)
+          .signers([message]) // appears to get erased when calling transaction()  - TODO remove?
+          .transaction();
+        
+        try {
+          await provider.sendAndConfirm(tx, [message]);
+        } catch (e) {
+            const anchorErrLog = e.logs.filter((elog) => elog.includes("AnchorError"));
+            expect(anchorErrLog).to.have.lengthOf(1);
+            chai.assert.include(anchorErrLog[0], "IncorrectDestinationAccount");
+        }
+          
+      });
+    });
+});
